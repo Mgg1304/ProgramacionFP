32ffd1d908a0997576f801c8a57775840f3729c4
Mgg1304 <migargar04@gmail.com>
2024-09-24 14:15:55 +0200

    x

--- Tema 1/Actividad2/src/ifElse/Ejer8.java
+++ Tema 1/Actividad2/src/ifElse/Ejer8.java
@@ -1,5 +1,14 @@
 package ifElse;
 
-public class Ejer8 {
+import java.util.Scanner;
 
+public class Ejer8 {
+	public static Scanner sc = new Scanner(System.in);
+	
+	/*
+	 * Ejercicio 8: Escribe un programa que pida dos numeros y te devuelva si el mayor es multiplo del menor
+	 * */
+	public static void main(String[] args) {
+		
+	}
 }
