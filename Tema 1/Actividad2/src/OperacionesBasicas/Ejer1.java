8568d334791edbf30844188bf7f20003d893c682
Mgg1304 <migargar04@gmail.com>
2024-09-24 13:09:26 +0200

    x

--- Tema 1/Actividad2/src/OperacionesBasicas/Ejer1.java
+++ Tema 1/Actividad2/src/OperacionesBasicas/Ejer1.java
@@ -1,10 +1,29 @@
 package OperacionesBasicas;
 
+import java.util.Scanner;
+
 public class Ejer1 {
-
+	
+	public static Scanner sc = new Scanner (System.in);
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		float n1, n2, n3;
+		
+		System.out.println("CALCULO DE MEDIA ARITMETICA");
+		
+		//Leo el primer numero 
+		System.out.println("Dame el primer numero: ");
+		n1 = sc.nextInt();
+		
+		//Leo el segundo numero
+		System.out.println("Dame el segundo numero: ");
+		n2 = sc.nextInt();
+		
+		//Hago la media aritmetica
+		n3 = (n1 + n2)/2;
+		System.out.println("La media aritmetica de " +n1+ " y de " +n2+ " es " + n3);
+		
 	}
 
 }
