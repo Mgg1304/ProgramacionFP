0be706e31195200c6744e6237372f9861c0d357a
Mgg1304 <migargar04@gmail.com>
2024-10-11 11:20:16 +0200

    x

---
+++ Tema 1/Actividad_2_ProgramaciónEstructurada/src/Ejer_30_35/Ejer30_1.java
@@ -0,0 +1,19 @@
+package Ejer_30_35;
+
+import java.util.Scanner;
+
+public class Ejer30_1 {
+
+	public static Scanner sc = new Scanner(System.in);
+
+	/**
+	 * Diseñar el algoritmo que indique el mayor de una serie de números que
+	 * introducimos por teclado. Crea dos versiones: en la primera la cantidad está
+	 * definida en la segunda será indefinida.
+	 */
+	public static void main(String[] args) {
+		
+		int num, max, min, numElementos;
+	}
+
+}
