c5a99acf3a045c87d077da012c8f1d95098a8eab
Mgg1304 <migargar04@gmail.com>
2024-10-25 10:55:23 +0200

    x

---
+++ Tema 2/Actividad1_Programación_Modular/src/FuncionesNumero/LibNumerosEnteros.java
@@ -0,0 +1,45 @@
+package FuncionesNumero;
+
+public class LibNumerosEnteros {
+
+	/**
+	 * Calcula la suma de las cifras de un numero
+	 * 
+	 * @param num: entero
+	 * @return int
+	 */
+	public static int sumaCifrasNumero(int num) {
+		return 0;
+	}
+
+	/**
+	 * Comprueba si un numero es primo
+	 * 
+	 * @param num: entero
+	 * @return boolean
+	 */
+	public static boolean esPrimo(int num) {
+		return false;
+	}
+
+	/**
+	 * Comprueba si un numero es capicua
+	 * 
+	 * @param num: entero
+	 * @return boolean
+	 */
+	public static boolean esCapicua(int num) {
+		return false;
+	}
+
+	/**
+	 * Comprueba si un numero tiene sus cifras en orden creciente
+	 * 
+	 * @param num: entero
+	 * @return boolean
+	 */
+	public static boolean cifrasOrdenCreciente(int num) {
+		return false;
+	}
+
+}
